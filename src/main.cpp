d97d9331700e59a9c2ca1f2909d48b9806db81d2
powerof3 <32599957+powerof3@users.noreply.github.com>
2023-03-26 04:21:09 +0530

    Support 1.6.353-

--- src/main.cpp
+++ src/main.cpp
@@ -115,8 +115,7 @@ extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
 	v.PluginVersion(Version::MAJOR);
 	v.PluginName("Seasons Of Skyrim");
 	v.AuthorName("powerofthree");
-	v.UsesAddressLibrary();
-	v.UsesUpdatedStructs();
+	v.UsesAddressLibrary(true);
 	v.CompatibleVersions({ SKSE::RUNTIME_LATEST });
 
 	return v;
